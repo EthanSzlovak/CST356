da92bc7c45eb35fc4adb77e8c6cb4bdd8638839d
EthanSzlovak <43588501+EthanSzlovak@users.noreply.github.com>
2020-02-07 21:57:30 -0800

    Done

--- Week4/js/login.js
+++ Week4/js/login.js
@@ -1,16 +1,16 @@
 function validate() {
     
-    document.getElementById('first-name-error').style.visibility = 'hidden'
-    document.getElementById('password-error').style.visibility = 'hidden'
+    document.getElementById('first-name-error').style.visibility = 'hidden';
+    document.getElementById('password-error').style.visibility = 'hidden';
 
-    let username = document.getElementById('first-name').value
-    let password = document.getElementById('password').value
+    var username = document.getElementById('first-name').value;
+    var password = document.getElementById('password').value;
 
     if (username == '') {
-        document.getElementById('first-name-error').style.visibility = 'visible'
+        document.getElementById('first-name-error').style.visibility = 'visible';
     }
 
     if (password == '') {
-        document.getElementById('password-error').style.visibility = 'visible'
+        document.getElementById('password-error').style.visibility = 'visible';
     }
 }
